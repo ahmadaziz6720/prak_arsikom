c25dcd88f50ef3dab26e58a0693f702597c60d51
ahmadaziz6720 <a715210@gmail.com>
2022-09-23 14:55:19 +0700

    tugas 8

---
+++ modul_1/1_Lab/Tugas_8/main.c
@@ -0,0 +1,27 @@
+// Praktikum EL3111 Arsitektur Sistem Komputer
+// Modul : 1
+// Percobaan : 8
+// Tanggal : 23 September 2022
+// Kelompok : 10
+// Rombongan : B
+// Nama (NIM) 1 : Ahmad Aziz (13220034)
+// Nama (NIM) 2 : Gilbert Ng (13220032)
+// Nama File : main.c
+// Deskripsi : Demonstrasi procedure call dan stack
+// Menghitung jumlah dari kuadrat bilangan
+
+int square (int x){
+    return x*x;
+}
+
+int squaresum (int y, int z){
+    int temp1 = square(y);
+    int temp2 = square(z);
+    return temp1+temp2;
+}
+
+int main (void){
+    int a = 5;
+    int b = 9;
+    return squaresum(a,b);
+}
