d76e51c509b88a13d536042b727ebbb6ad96d5a3
ahmadaziz6720 <a715210@gmail.com>
2022-10-06 11:02:22 +0700

    Done tugas 2

---
+++ EL3111_2_20221006_13220034/1_Lab/Tugas_2/getByte.h
@@ -0,0 +1,15 @@
+// Praktikum EL3111 Arsitektur Sistem Komputer
+// Modul : 2
+// Percobaan : 1
+// Tanggal : 6 Oktober 2022
+// Kelompok : 10
+// Rombongan : B
+// Nama (NIM) 1 : Ahmad Aziz (13220034)
+// Nama (NIM) 2 : Gilbert Ng (13220032)
+// Nama File : getByte.h
+// Deskripsi : getByte
+
+
+#include <stdio.h>
+
+int getByte (int x, int n);
---
+++ EL3111_2_20221006_13220034/1_Lab/Tugas_2/main.c
@@ -0,0 +1,28 @@
+// Praktikum EL3111 Arsitektur Sistem Komputer
+// Modul : 2
+// Percobaan : 1
+// Tanggal : 6 Oktober 2022
+// Kelompok : 10
+// Rombongan : B
+// Nama (NIM) 1 : Ahmad Aziz (13220034)
+// Nama (NIM) 2 : Gilbert Ng (13220032)
+// Nama File : main.c
+// Deskripsi : getByte
+
+#include "getByte.h"
+#include "printbitbyte/printbitbyte.h"
+
+int main() {
+    int x;
+    int n;
+    printf("Masukkan nilai x: ");
+    scanf("%x", &x);
+    printf("Masukkan nilai n: ");
+    scanf("%d", &n);
+
+    printf("Byte x = 0x "); printByte((byte_pointer) &x, sizeof(x));
+
+    int z = getByte(x, n);
+    printf("byte ke %d = 0x%x", n, z);
+    return 0;
+}
