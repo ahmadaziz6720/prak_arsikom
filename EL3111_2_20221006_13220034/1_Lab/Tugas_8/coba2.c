1fcc122424208d70612fe918d5ede050e8ceefd7
ahmadaziz6720 <a715210@gmail.com>
2022-10-06 15:32:10 +0700

    bikin coba 2

---
+++ EL3111_2_20221006_13220034/1_Lab/Tugas_8/coba2.c
@@ -0,0 +1,26 @@
+// Praktikum EL3111 Arsitektur Sistem Komputer
+// Modul : 2
+// Percobaan : 1
+// Tanggal : 6 Oktober 2022
+// Kelompok : 10
+// Rombongan : B
+// Nama (NIM) 1 : Ahmad Aziz (13220034)
+// Nama (NIM) 2 : Gilbert Ng (13220032)
+// Nama File : getByte.c
+// Deskripsi : getByte
+
+void coba(int* x, int* y, int* z){
+    // Kamus
+    double a;
+    double b;
+    double c;
+    double d;
+    // Algoritma
+    a = *x;
+    b = *y;
+    c = *z;
+    d = a+b;
+    *y = d;
+    *z = b;
+    *x = c;
+}
